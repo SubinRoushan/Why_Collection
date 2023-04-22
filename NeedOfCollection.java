1884d6fc40bad38701e3384b430c42b81a8eda59
SubinRoushan <129893487+SubinRoushan@users.noreply.github.com>
2023-04-22 13:21:37 +0530

    Add files via upload

---
+++ NeedOfCollection.java
@@ -0,0 +1,21 @@
+package com.java.collection;
+
+public class NeedOfCollection {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=20;
+        int c=30;
+        int d=40;
+        int e=50;
+
+        //one objects can store many variables
+        int[] arr=new int[10];
+
+        //Limitations of Array
+        //1. Arrays are fixed
+        //2. Arrays are hemogeneous data elements (same datatype)
+        //3. Ready made APIs support is not avilable in arrays
+        // That's why Collection came in market
+    }
+}
